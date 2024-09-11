d8a982899b2921f2d585f1bae64efd3bbba38aa9
Ayush Deb <ayushdeb848@gmail.com>
2024-09-11 17:46:19 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,14 @@
 import type { Metadata } from "next";
 import {IBM_Plex_Sans} from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const IBMPlex = IBM_Plex_Sans({ 
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex'
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 
 export const metadata: Metadata = {
   title: "Pixel Craft",
